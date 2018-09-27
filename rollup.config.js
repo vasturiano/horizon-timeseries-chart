7c61e340794cb446483fdf7ab631696674ae19e2
vasco <vastur@gmail.com>
2018-09-26 22:12:29 -0700

    Add module bundle config

---
+++ rollup.config.js
@@ -0,0 +1,31 @@
+import resolve from 'rollup-plugin-node-resolve';
+import commonJs from 'rollup-plugin-commonjs';
+import postCss from 'rollup-plugin-postcss';
+import postCssSimpleVars from 'postcss-simple-vars';
+import postCssNested from 'postcss-nested';
+import babel from 'rollup-plugin-babel';
+import { name, homepage, version } from './package.json';
+
+export default {
+  input: 'src/index.js',
+  output: [
+    {
+      format: 'umd',
+      name: 'HorizonTSChart',
+      file: `dist/${name}.js`,
+      sourcemap: true,
+      banner: `// Version ${version} ${name} - ${homepage}`
+    }
+  ],
+  plugins: [
+    postCss({
+      plugins: [
+        postCssSimpleVars(),
+        postCssNested()
+      ]
+    }),
+    resolve(),
+    commonJs(),
+    babel({ exclude: 'node_modules/**' })
+  ]
+};
